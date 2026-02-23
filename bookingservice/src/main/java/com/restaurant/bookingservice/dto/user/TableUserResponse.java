e50fc332fa759c2916c92dc6d3a9f1c6d4cbf04d
Shurriik <alexander.rat.88@mail.ru>
2026-02-23 23:48:09 +0300

    Add files via upload

---
+++ bookingservice/src/main/java/com/restaurant/bookingservice/dto/user/TableUserResponse.java
@@ -0,0 +1,20 @@
+package com.restaurant.bookingservice.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableUserResponse {
+    private Long id;
+    private Integer number;
+    private Integer capacity;
+    private String location;
+    private String description;
+    private boolean available;
+}
